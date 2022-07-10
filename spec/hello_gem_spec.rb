57c055c6769b3a28b2b02244d4afffc2a4f79da1
Takahito Urai <uraitakahito@gmail.com>
2022-07-10 13:28:12 +0900

    add sample method and rspec

--- spec/hello_gem_spec.rb
+++ spec/hello_gem_spec.rb
@@ -2,4 +2,10 @@ RSpec.describe HelloGem do
   it "has a version number" do
     expect(HelloGem::VERSION).not_to be nil
   end
+
+  describe '#greet' do
+    it 'returns "Hello World!"' do
+      expect(described_class.greet).to eq('Hello World!')
+    end
+  end
 end
