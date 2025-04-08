98525b1cb27a3a82fd5f9295a3b889ce7187a8f6
Takahito Urai <uraitakahito@gmail.com>
2025-04-08 15:37:38 +0900

    rbenv exec bundle gem hello_gem

---
+++ lib/hello_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hello_gem/version"
+
+module HelloGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hello_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HelloGem do
+  it "has a version number" do
+    expect(HelloGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
